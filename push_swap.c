f3be200612514c23604c4cc4f42889e955f14036
Ahmad Alwi Syed <asyed@c1r8s8.42singapore.sg>
2024-04-25 18:34:05 +0800

    test main

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:22:28 by asyed             #+#    #+#             */
-/*   Updated: 2024/04/20 15:56:11 by asyed            ###   ########.fr       */
+/*   Updated: 2024/04/25 18:32:24 by asyed            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -30,7 +30,9 @@
     // beyond range
     // not int
 
-int checkduplicate(char *arr, char *nextarr)
+/*
+
+int checkduplicate(char *intargs)
 
 {
     while (*arr)
@@ -47,47 +49,43 @@ int checkduplicate(char *arr, char *nextarr)
     return (0); 
 }
 
-int checkdigits(char *arr, char *nextarr)
+*/
+
+
+int notdigits(char *intargs)
 
 {
-    while (*arr)
+    while (*intargs)
     {
-        while (*nextarr)
+        if (intargs < '0' || intargs > '9') // if digits, return true
         {
-            if (arr >= '0' && arr <= '9') // if digits, return true
-            {
-                return(1);
-            }
+            return(1);
         }
-
+        intargs++;
     }
-    return (0); 
+    return (0);
 }
 
 int main(int ac, char *av[])
 {
-    int firstarray;
-    int nextarray;
-    int index;
+    int start;
+    char    **intargs;
+    start = 1;
+    //intargs = av + 1; // pointer to whole str
 
-    firstarray = 1;
-    nextarray = firstarray + 1;
-    index = 0;
-
-    if (ac == 1) // no parameter
-    {
-        // display prompt back
-    }
     if (ac > 1) // multiple argv
     {
-        while (av) // traverse through args
+        while (start < ac - 1) // traverse through args
         {
-            if (checkdigits(av[firstarray], av[nextarray]) && checkduplicate(av[firstarray], av[nextarray])) // check digits and check doubles
+            if (notdigits(av[start]))// || gotduplicate(intargs[start])) // check digits and check doubles
             {
-                ft_atoi(av); // convert ascii to integer
+                ft_putstr_fd("Error\n", 2); // how to do it on standard error (fd2), 2nd parameer     
             }
+            start++;
         }
-        
+        // error checking then populate
+        ft_atoi(av); // convert ascii to integer, what if atoi have INTMAX OR INTMIN
     }
-    ft_printf("Error\n"); // how to do it on standard error (fd2)?
+    return (0); // for no parameter
+    
 }
